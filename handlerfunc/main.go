f3528bd90881b647be9e36b8b6e5866242ddaf2f
Willem Schots <w@willem.dev>
2023-08-27 10:42:23 +0200

    move documentation to single file

--- handlerfunc/main.go
+++ handlerfunc/main.go
@@ -1,3 +1,19 @@
+// # Using http.HandlerFunc
+//
+// This command runs a http handler using http.HandlerFunc. This is a small demo related to my [article on handler func] 👨‍🎤.
+//
+// There is an executable `main.go` that contains a demo.
+//
+// # Notes
+//
+//   - Functions as values.
+//   - Function types that call themselves to implement an interface.
+//   - Use compiler errors to guide explanation.
+//   - http.HandlerFunc and http.HandleFunc have very similar names and can be confused.
+//   - http.HandlerFunc(...) looks like a function call but it's a type conversion.
+//   - http.HandlerFunc implements http.Handler by calling itself. This can be a bit trippy for newcomers.
+//
+// [article on handler func]: https://www.willem.dev/articles/http-handler-func/
 package main
 
 import (
